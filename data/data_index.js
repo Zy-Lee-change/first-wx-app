bb79357d437aa37225139c124e979dfca2efb98d
Zy-Lee-change <502664745@qq.com>
2018-07-19 11:12:28 +0800

    first commit

---
+++ data/data_index.js
@@ -0,0 +1,112 @@
+/**
+ * Created by Rebecca_Han on 16/10/26.
+ */
+module.exports = {
+
+}
+
+var index= {
+    "id": 1,
+        "data": [
+        {
+            "question_id": 1,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "董不懂",
+            "feed_source_txt": "",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "每人进来说一个最近心里暗爽美滋滋又不方便对别人说的事吧",
+            "answer_ctnt": "有时候自己身上发生了什么好事，自己美的不行，但是又不好和好朋友们分享，怕打击到别人，让人以为在臭显摆或者高姿态有优越感什么的，只能自己偷着乐，但是好不爽啊，好想说出来！就在这里说吧！哈哈。 我先说：老公国内t...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 2,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "Alex",
+            "feed_source_txt": "",
+            "feed_source_img": "../../images/icon8.jpg",
+            "question": "极简阶段性感想",
+            "answer_ctnt": "先来骂醒一些人（包括我自己），入1出1是用在你身上的么？！家人囤货是你不极简的理由么？！别来问我们该不该买什么，不该！你杂七杂八那么多，还好意思买买买？！你东西还没精简到目标数量，就敢开始升级？！。。。 我，...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 3,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "George",
+            "feed_source_txt": "",
+            "feed_source_img": "../../images/icon9.jpeg",
+            "question": "你们有哪些喜欢的关于夏天的描写？",
+            "answer_ctnt": "今天看《目送》特别喜欢龙应台写童年学校的那段 学校外面有野溪，被浓密的热带植物沿岸覆盖，莓果的香甜气息混在空气里，令人充满莫名的幸福感。溪水清澈如许，赤足其中，低头便可见透明的细虾和黑油油的蝌蚪在石头间游走...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 4,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "NotThis",
+            "feed_source_txt": "",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "说说你们抠门的具体表现，有多过分（野生）",
+            "answer_ctnt": "先说我自己的吧，lz金牛座，动手能力特别强 发现网易新闻签到阅读评论这些都有金币，金币可以兑换试用装。今天兑换的就送到了，感觉好开心，白来的特别棒，噢耶！ 都说外甥像舅舅，我想...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 5,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "YourTown",
+            "feed_source_txt": "",
+            "feed_source_img": "../../images/icon8.jpg",
+            "question": "晋江入门小白的一点经验",
+            "answer_ctnt": "刚好看到首页在飘关于jj的帖子 想聊下新人的一点心得吧。 lz本人算细胞，虽然了解一些情况，但是成绩real扑。 附图，大概17年初来晋江的，完结三本正在连载一本才到这个程度，算非常一般的程...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 6,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "Maya",
+            "feed_source_txt": "",
+            "feed_source_img": "../../images/icon9.jpeg",
+            "question": "大家来说说第一次给自己带来巨大震撼的科幻片吧！",
+            "answer_ctnt": "我自己先来，我记得是小时候看的，好像叫做《黑洞表面》，迷失在太空中的飞船经历了一些完全超乎人类想象的经历，最后留下的是黑匣子里一段舰队成员死前的留言和一船恐怖的遗骸，导致从小脑子就有个大洞的我吓得不轻，现在...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 7,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "Mamami",
+            "feed_source_txt": "",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "California-一号公路",
+            "answer_ctnt": "最近在加州 因为要整理下手机内存 就索性放些这两天拍的照片吧。 因为希望最近的照片更偏向于生活些，就降低了饱和度和对比等。以营造更全面平和的画面。 以前的则更浓墨重彩，都关乎于...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 8,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "Ken",
+            "feed_source_txt": "",
+            "feed_source_img": "../../images/icon8.jpg",
+            "question": "我的小人活活笑死了……",
+            "answer_ctnt": "只是听了另一个谐星小人讲的全世界最好笑的笑话，就笑到歇斯底里，赶紧不交谈了让她去吃饭，还没走到就突然提示说活活笑死了！！！！！！！！！！ 加一个另一天游完泳上...",
+            "good_num": "112",
+            "comment_num": "18"
+        }
+
+    ]
+
+}
+
+module.exports.index = index;
